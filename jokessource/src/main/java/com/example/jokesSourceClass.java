9cb6f9c6091856ab8b5da451d2ba318245ecf377
rajatkhanna <legalimpurity@gmail.com>
2017-09-01 12:59:55 +0530

    Step 1, Java library for jokes.

---
+++ jokessource/src/main/java/com/example/jokesSourceClass.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.Random;
+
+public class jokesSourceClass {
+
+    public static String getJoke(){
+        String[] jokearray =  new String[]{
+                "Q: What computer sings the best?\nA: A Dell.",
+                "Q: How many light bulbs does it take to change a light bulb?\nA: They can't; they're not bright enough.",
+                "I put so much more effort into naming my first Wi-Fi than my first child.",
+                "The cool part about naming your kid is you don’t have to add six numbers to make sure the name is available."
+        };
+
+        Random rn = new Random();
+        return jokearray[rn.nextInt(jokearray.length)];
+    }
+
+}
