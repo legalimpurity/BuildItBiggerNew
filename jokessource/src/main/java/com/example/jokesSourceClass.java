c001002b1ddd4c829f9ca609c6db545603bd0400
rajatkhanna <legalimpurity@gmail.com>
2017-09-03 12:02:45 +0530

    I had understood the task wrongly. the dependency was to be made between the jokessource library and GCE in the opposite direction. FIxing the whole setup, now I also would be able to use the Asynctask. Moved back to stage 2 commit. Will have to do all tasks again.

--- jokessource/src/main/java/com/example/jokesSourceClass.java
+++ jokessource/src/main/java/com/example/jokesSourceClass.java
@@ -9,6 +9,7 @@ public class jokesSourceClass {
                 "Q: What computer sings the best?\nA: A Dell.",
                 "Q: How many light bulbs does it take to change a light bulb?\nA: They can't; they're not bright enough.",
                 "I put so much more effort into naming my first Wi-Fi than my first child.",
+                "The cool part about naming your kid is you don’t have to add six numbers to make sure the name is available.",
                 "The cool part about naming your kid is you don’t have to add six numbers to make sure the name is available."
         };
 
